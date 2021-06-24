9523b518c571b7cc6b5b14d82d33e5caa8a1a337
Pavlo Marii <pavlomarii@gmail.com>
2021-06-24 13:25:18 +0300

    Added comments, remove debug and update Readme

--- tests/test_app_routes.py
+++ tests/test_app_routes.py
@@ -2,12 +2,14 @@ import http
 from app import app
 
 
+# Test root route of application with index page.
 def test_root():
     client = app.test_client()
     resp = client.get('/')
     assert resp.status_code == http.HTTPStatus.OK
 
 
+# Test /graphql get background page.
 def test_graphql():
     client = app.test_client()
     resp = client.get('/graphql')
